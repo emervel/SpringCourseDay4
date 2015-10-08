2a9dbd8a8a48d3ba0ca251fdb720e4efbe54a0df
emervel <eduardomerchanvelasco@gmail.com>
2015-10-08 19:42:58 +0200

    Primer commit

---
+++ SpringDaoHibernate/src/main/java/com/curso/springdaohibernate/dao/PersonaDao.java
@@ -0,0 +1,33 @@
+package com.curso.springdaohibernate.dao;
+
+import com.curso.springdaohibernate.dominio.Aficion;
+import com.curso.springdaohibernate.dominio.Persona;
+
+import java.util.List;
+
+/**
+ * Interfaz que establecerá los metodos a ser utilizados por el DAO
+ * @author Emervel
+ */
+public interface PersonaDao {
+
+    List<Persona> getPersonas();
+
+    Persona insertPersona(Persona una);
+
+    void updatePersona(Persona una);
+
+    void deletePersona(Persona una);
+
+    Persona getPersona(Integer id);
+
+    List<Persona> getPorNombreParecido(String nombre);
+
+    Long getNumeroDePersonas();
+
+    List<Aficion> getAficiones(Integer id);
+
+    List<Aficion> getAficionesDeOtraManera(Integer id);
+
+    List<Aficion> getAficionesDeOtraManeraMas(Integer id);
+}
